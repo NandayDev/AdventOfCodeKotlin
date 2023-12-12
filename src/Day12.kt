51301dab097384f3ffb67b2f2022558724c9f509
Guido <guido.cardinali@gmail.com>
2023-12-12 10:15:42 +0100

    Day 12 wip

---
+++ src/Day12.kt
@@ -0,0 +1,21 @@
+fun main() {
+    fun part1(input: List<String>): String {
+
+
+
+        return ""
+    }
+
+    fun part2(input: List<String>): String {
+        return ""
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day12_test")
+    check(part1(testInput) == "21")
+    check(part2(testInput) == "")
+
+    val input = readInput("Day12")
+    part1(input).println()
+    part2(input).println()
+}
